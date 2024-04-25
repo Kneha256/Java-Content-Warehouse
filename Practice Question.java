e9aa401c402ca457d1aff4fbd21814f4f1ae7663
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-04-25 13:45:14 +0530

    Update Practice Question.java

--- Practice Question.java
+++ Practice Question.java
@@ -1,1 +1,10 @@
-
+public class Prime_Number{
+    public static void main(String[] args){
+      int n=5;
+        if(n%n==0){
+          System.out.println("prime");}
+        else{
+           System.out.println("non prime");
+        }
+      }
+}
