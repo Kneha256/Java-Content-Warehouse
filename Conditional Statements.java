bd85355a77d6804f0ee0d5d9d391e4dc4de91f2a
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-04 08:15:28 +0530

    Update Conditional Statements.java

--- Conditional Statements.java
+++ Conditional Statements.java
@@ -73,7 +73,30 @@ class GREETING {
         }
         else if(button==3){
             System.out.println("Bonjour");
-        }       
+        } 
+        else{
+            System.out.println("Invalid button");
+        }
+ }
+}
+
+
+
+
+import java.util.*;
+class GREETING {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int button=sc.nextInt();
+        switch(button){
+            case 1:System.out.println("Hello");
+            break;
+            case 2:System.out.println("Namaste");
+            break;
+            case 3:System.out.println("Bonjor");
+            break;
+            default:System.out.println("Invalid input");
+        }
  }
 }
 
