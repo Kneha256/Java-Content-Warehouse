dbcce514ec31fb6fa935095541404ed163df1c35
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-04 07:34:15 +0530

    Update My First Java Program.java

--- My First Java Program.java
+++ My First Java Program.java
@@ -3,3 +3,17 @@ public class FirstJavaCode{
         System.out.println("My First Java Code");
     }
 }
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>=18){
+             System.out.println("Can Vote");
+        }
+        else{
+            System.out.println("Cannot Vote");
+                  }
+    }
+}
