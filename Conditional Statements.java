6f54cc5d3386f160edb139779d483d7f756b09c1
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-08 01:48:45 +0530

    Update Conditional Statements.java

--- Conditional Statements.java
+++ Conditional Statements.java
@@ -32,15 +32,15 @@ class num_calc {
     int a=sc.nextInt();
     int b=sc.nextInt();
     if(a==b){
-    System.out.println("equal");
-    }
-        else if(a>b){
+       System.out.println("equal");
+}
+    else if(a>b){
              System.out.println("a is greater");
-        }
-        else{
-            System.out.println("a is lesser");
-                }
-    }
+}
+    else{
+             System.out.println("a is lesser");
+ }
+}
 }
 
 
