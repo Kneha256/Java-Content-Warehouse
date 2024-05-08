ab0082178089921e8085ef73a6b02e2acd7b9e02
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-08 08:55:59 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -12,3 +12,70 @@ public class arraydefine{
         }
         }
 }
+
+
+import java.util.*;
+public class IndexNo{
+  public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+        int[] arr=new int[size];
+    int n=20;
+        for(int i=0;i<size;i++){
+          arr[i]=sc.nextInt();
+        }
+    for(int i=0;i<size;i++){
+            if(arr[i]==n){
+                 System.out.println(i);
+            }
+     }
+  }
+}
+
+
+
+
+
+import java.util.*;
+public class IndexNo{
+  public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+    
+      for(int i=0;i<size;i++){
+          arr[i]=sc.nextInt();
+        }
+     int num=sc.nextInt();
+     for(int i=0;i<size;i++){
+            if(arr[i]==n){
+                 System.out.println(i);
+            }
+     }
+  }
+}
+     //or
+
+import java.util.*;
+public class IndexNo{
+  public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+    
+      for(int i=0;i<arr.length;i++){
+          arr[i]=sc.nextInt();
+        }
+     int num=sc.nextInt();
+     for(int i=0;i<arr.length;i++){
+            if(arr[i]==n){
+                 System.out.println(i);
+            }
+     }
+  }
+}
+
+
+
+
+
