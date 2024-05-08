db0c5cf6fb7eb1e98b0892ad0b7f4909782e5bbb
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-08 10:14:55 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -99,3 +99,22 @@ public class Arr{
     
     
 
+import java.util.*;
+public class Arr{
+  public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+    int row=sc.nextInt();
+        int col=sc.nextInt();
+    int[][] arr=new int[row][col];
+        for(int i=0;i<row;i++){
+          for(int j=0;j<col;j++){
+                arr[i][j] = sc.nextInt();
+                             }
+        }
+    int n=sc.nextInt();
+        for(int i=0;i<row;i++){
+          for(int j=0;j<col;j++){
+               if(arr[i][j] == n) {
+                  System.out.println(i+","+j);
+               }
+          }
