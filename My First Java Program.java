a47b0c4896d0041d258bcc8289d2d65dc5f5a61d
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-31 22:23:25 +0530

    Update My First Java Program.java

--- My First Java Program.java
+++ My First Java Program.java
@@ -1,7 +1,9 @@
-public class FirstJavaCode{
-    public static void main(String[] args){
+public class FirstJavaCode
+{
+    public static void main(String[] args)
+{
         System.out.println("My First Java Code");
-    }
+}
 }
 
 
