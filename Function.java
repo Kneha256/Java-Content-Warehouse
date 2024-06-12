18d75b7c11aecce154beb0ccef95e56c33c12c6d
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-06-12 23:55:28 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -3,6 +3,7 @@ public class Sum {
   public static int Sum_cal(int a, int b){
   int sum = a+b;
   return sum;
+    
 }                                                
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
