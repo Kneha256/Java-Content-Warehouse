9ca467c857f683bbcfe29e52510bd2d831be6b2f
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-06-29 02:14:36 +0530

    Update practice question.java

--- practice question.java
+++ practice question.java
@@ -19,3 +19,17 @@ public class sum{
   int sum=a+b;
         System.out.println(sum);}}
 
+import java.util.*;
+public class Food{
+public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      switch(a){
+      case 1:System.out.println("jalei");
+        break;
+      case 2:System.out.println("rasogulla");
+        break;
+      default:System.out.println("no food");}
+        }}
+
+
