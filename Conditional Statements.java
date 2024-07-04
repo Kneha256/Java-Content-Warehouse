efa12cf1410a0516759d46293da7142492604979
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-07-04 14:32:35 +0530

    Update Conditional Statements.java

--- Conditional Statements.java
+++ Conditional Statements.java
@@ -164,11 +164,30 @@ public class positive_negative{
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         if(a>0){
-            System.out.println("it is a positive number);
+            System.out.println("it is a positive number");
         }
         else{
-            System.out.println("it is a negative number);
+            System.out.println("it is a negative number");
         }
         
     }}
+
+
+
+import java.util.*;
+public class fever_detector{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float temp=sc.nextFloat();
+        if(temp>100){
+            System.out.println("you have fever");
+        }
+        else{
+            System.out.println("no fever");
+        }
+        
+    }}
+
+
+
 
