1c349f4926ed5e13bb52f7cc2019ef78b164190a
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-09-07 00:38:08 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -80,3 +80,22 @@ public class HelloWorld{
         
     }
 }
+
+
+import java.util.*;
+public class HelloWorld{
+    public static int calavg(int a,int b, int c){
+        return (a+b+c)/3;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int avg=calavg(a,b,c);
+        System.out.println(avg);
+    }
+}
+
+
+
