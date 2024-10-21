fc071ca58f27106a828cd1c819b212955d6826f9
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-10-21 19:04:09 +0530

    Update Function and Methods.java

--- Function and Methods.java
+++ Function and Methods.java
@@ -4,6 +4,7 @@ public class JavaBasics{
         return (a+b+c)/3;
         
     }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
