801df8beaccdee11d657f676d2b0328eedbff6f7
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-01-20 16:52:37 +0530

    Update arrayCC.java

--- arrayCC.java
+++ arrayCC.java
@@ -96,3 +96,24 @@ public class Hello {
       
   }
 }
+
+
+#printPairs of given array
+    import java.util.*;
+
+public class Main {
+    public static void Pair(int numbers[]){
+    for(int i=0;i<numbers.length;i++){
+      int curr=numbers[i];
+      for(int j=i+1;j<numbers.length;j++){
+        System.out.print("(" + curr + "," + numbers[j] + ")");
+      }
+      System.out.println();
+    }
+      
+    }
+    public static void main(String[] args) {
+     int numbers[]={1,2,3,4,5,6};
+     Pair(numbers);
+  }
+}
