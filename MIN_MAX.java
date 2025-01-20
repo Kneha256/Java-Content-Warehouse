0c821a1dfbac2e8da4a6ec26768e4f864b6d5301
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-01-20 16:29:16 +0530

    Update MIN_MAX.java

--- MIN_MAX.java
+++ MIN_MAX.java
@@ -1,3 +1,4 @@
+//to find minimum value
 import java.util.*;
 
 public class Main {
@@ -13,5 +14,26 @@ public class Main {
     public static void main(String[] args) {
      int numbers[]={8,2,4,5,3,6,7,9};
      System.out.println("Minimum value is : " + getMinimum(numbers));
+  }
+}
+
+//To find Max value in the given array
+import java.util.*;
+
+public class Hello {
+    public static int getvalue(int numbers[]){
+      int largest = Integer.MIN_VALUE;
+      for(int i=0;i<numbers.length;i++){
+        if(numbers[i]>largest){
+          largest=numbers[i];
+        }
+      }
+      return largest;
+    }
+    
+    public static void main(String[] args) {
+      int numbers[]={2,3,4,9,5,8};
+      System.out.println("largest number is : "+getvalue(numbers));
+      
   }
 }
