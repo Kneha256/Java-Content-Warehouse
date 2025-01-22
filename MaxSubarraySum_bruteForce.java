e4257b4cc6875594adbb879b94082dd9a3b491d2
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-01-22 18:13:16 +0530

    Update MaxSubarraySum_bruteForce.java

--- MaxSubarraySum_bruteForce.java
+++ MaxSubarraySum_bruteForce.java
@@ -26,3 +26,34 @@ public class Main {
       
   }
 }
+
+
+
+#practice
+  import java.util.*;
+
+public class Main {
+  public static void maxsubarraysum(int number[]){
+    int current=0;
+    int Max=Integer.MIN_VALUE;
+    for(int i=0;i<number.length;i++){
+      for(int j=0;j<number.length;j++){
+        current=0;
+        for(int k=i;k<=j;k++){
+          current+=number[k];
+          }
+          System.out.println(current);
+          if(current>Max){
+            Max=current;
+            
+          }
+        
+        }
+      }
+      System.out.print("max sub array sum : "+Max);
+    }
+    public static void main(String[] args) {
+      int number[]={1,-2,6,-1,3};
+      maxsubarraysum(number);
+  }
+}
