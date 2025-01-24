eb7e9292c320dfdb19de78122607fedc16531754
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-01-24 10:19:17 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -25,3 +25,36 @@ public class Main {
       printarr(arr);
       }
 }
+
+
+
+#practice with another arr
+    import java.util.*;
+
+public class Main {
+    public static void bubblesort(int arr[]){
+      for (int i=0;i<arr.length-1;i++){
+        for (int j=0;j<arr.length-1-i;j++){
+          if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            
+          }
+        }
+        
+      }
+    }
+    public static void printarr(int arr[]){
+      for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+      }
+    }
+    
+    public static void main(String[] args) {
+      int arr[]={6,4,7,3,5,2,1};
+      bubblesort(arr);
+      printarr(arr);
+      
+  }
+}
